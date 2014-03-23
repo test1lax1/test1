f1393d75368d87e0081a5e0f0678710977c0d6cb
laxman <test1lax1@gmail.com>
2014-03-23 17:59:12 +0530

    Adding a new directory

---
+++ testpy2/fib/mod1.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
+
+class display():
+	
+	def dis():
+		print "laxmsn"
---
+++ testpy2/test/test.py
@@ -0,0 +1,16 @@
+import mod1
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
+
+def main():
+
+	a=mod1.display()
+        a.dis()
+
+	
+
+
+if __name__=="__main__":
+	main()
+
